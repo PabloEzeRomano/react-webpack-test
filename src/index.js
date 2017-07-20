97ea061529da5d16ff963b4dc451209ad9c58158
Pablo Romano <pabloezeromano@gmail.com>
2017-07-20 10:07:08 -0300

    testing

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-export Button from './components/Button/Button';
-export Label from './components/Label/Label';
+import Button from './components/Button/Button';
+import Label from './components/Label/Label';
+
+const comps = { Button, Label };
+
+export default comps;
